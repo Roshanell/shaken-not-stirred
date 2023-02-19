f798a1f2375a7f097e728a2f49712fbba13b1710
Roshanell Francisco <roshanellf@gmail.com>
2023-02-19 14:42:56 -0600

    adding objective and inital function

---
+++ main.js
@@ -0,0 +1,9 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+document.querySelector("button").addEventListener("click", getDrink);
+
+function getDrink() {
+
+
+
+}
